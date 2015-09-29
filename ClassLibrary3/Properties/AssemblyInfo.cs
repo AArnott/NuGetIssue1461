2891a4e04499af9c75265dab3e344593fb03e90f
Andrew Arnott <andarno@microsoft.com>
2015-09-29 01:08:56 -0700

    Add two referencing projects
    ClassLibrary2 is exhibiting the double-import problem. It seems related to the content of the project.json file.

---
+++ ClassLibrary3/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClassLibrary3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary3")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
